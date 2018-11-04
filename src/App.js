ba859c15e85d2387c21df9750cd02f155d6425d3
laljiDP <laljiprajapati572@gmail.com>
2018-11-04 20:07:57 +0530

    design sidebar in admin side

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import LoginForm from './components/Login/index';
 import PrivateRoutes from './components/PrivateRoutes'
+import DashboardContainer from './containers/DashboardContainer';
 
 class App extends Component {
   render() {
     return (
       <Switch>
           <Route exact path="/" component={LoginForm} />
-          <PrivateRoutes exact path="/app/dashboard" component={() => <div>Dashboard</div>} />
+          <PrivateRoutes exact path="/app/dashboard" component={DashboardContainer} />
           <Route exact path="/About" render=  {() => <div> About us </div>} />
           <Route render={() => <div>404 Not Found</div>} />
       </Switch>
